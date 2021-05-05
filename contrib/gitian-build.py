5e89cef82222080355d4740e49d2c3141f7eda90
Krystof Pistek <krystof1119@gmail.com>
2021-05-05 18:28:04 +0200

    Fix lint error caused by my gitian-build.py modifications by removing unused imports.

--- contrib/gitian-build.py
+++ contrib/gitian-build.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-import argparse
-import os
-import subprocess
 import sys
 
 print('Gitian is currently UNSUPPORTED. It may become supported in the future.', file=sys.stderr)
